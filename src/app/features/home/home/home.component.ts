4322db7306bbc91822b5a273fb273f3f62acd095
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2022-05-25 04:25:39 +0200

    Update home.component.ts

--- src/app/features/home/home/home.component.ts
+++ src/app/features/home/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SidebarService } from 'src/app/shared/services/sidebar.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private sidebarService: SidebarService) {
+    this.sidebarService.setMenuItems([]);
+  }
 
   ngOnInit(): void {
+    console.log('ngOnInit home')
   }
 
 }
