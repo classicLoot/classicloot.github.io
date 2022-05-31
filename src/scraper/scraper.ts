74b03479d5ca02cfbe0bf663aaf577ee16f07ab7
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2022-06-01 01:41:17 +0200

    Update scraper.ts

--- src/scraper/scraper.ts
+++ src/scraper/scraper.ts
@@ -1,12 +1,15 @@
-import fs from 'fs';
-import fetch from 'node-fetch';
-import path from 'path';
 import { wotlkDungeons } from '../app/features/dungeons/data/wotlk';
 import { dungeon } from '../app/shared/interfaces/dungeons';
 
+// --------------
+const perSecond = 1;
+export const delay = 1000 / perSecond;
+
+export const XML_CONFIG = { compact: true };
+// --------------
+
 
 // get IDs
-
 const IDSet = new Set<number>();
 
 const dungeons: dungeon[] = wotlkDungeons;
@@ -18,42 +21,4 @@ dungeons.forEach(dng => {
         })
     })
 })
-
-// --------------
-const perSecond = 1;
-export const delay = 1000 / perSecond;
-
-export const XML_CONFIG = { compact: true };
-// --------------
-
-const IDArray = Array.from(IDSet.values());
-
-export const ItemIDArray: number[] = IDArray;
-
-console.log('to scrape: ' + IDArray.length);
-
-// -------------
-
-
-function checkFSIcon(icon: string) {
-    const itemPath = path.join(__dirname, '../assets/icons/', icon + '.gif');
-    const bExist = fs.existsSync(itemPath);
-    return bExist;
-}
-
-function writeFSIcon(icon: string) {
-    const itemPath = path.join(__dirname, '../assets/icons/', icon + '.gif');
-    fs.writeFileSync(itemPath, JSON.stringify(icon));
-}
-
-
-
-
-async function fetchIcons(icons: string[], size: string) {
-    for (const icon of icons) {
-        const response = await fetch(`https://wotlkdb.com/static/images/wow/icons/${size}/${icon}.jpg`);
-
-    }
-}
-
-fetchIcons(['spell_holy_summonchampion'], 'large')
+export const ItemIDArray: number[] = Array.from(IDSet.values());
