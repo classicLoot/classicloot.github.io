9cc83877a277a49180117c25cf4912cd1adcfe3b
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2022-06-01 03:10:30 +0200

    itemComponent

--- src/app/shared/components/wow-item/wow-item.component.ts
+++ src/app/shared/components/wow-item/wow-item.component.ts
@@ -1,17 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Observable } from 'rxjs';
+import { wowItem } from '../../interfaces/item';
+import { ItemdataService } from '../../services/itemdata.service';
 
 @Component({
   selector: 'app-wow-item',
   templateUrl: './wow-item.component.html',
   styleUrls: ['./wow-item.component.scss']
 })
-export class WowItemComponent implements OnInit {
+export class WowItemComponent implements OnInit, OnChanges {
 
   @Input() itemID: number = -1;
 
-  constructor() { }
+  public item$: Observable<wowItem>;
+
+  constructor(private itemDataService: ItemdataService) {
+    this.item$ = this.itemDataService.getItemData$(-1);
+  }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.item$ = this.itemDataService.getItemData$(this.itemID);
   }
 
 }
