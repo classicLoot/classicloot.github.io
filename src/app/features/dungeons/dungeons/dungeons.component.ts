e7b11f778c3711eeda4f1e1d83803f760ccb074f
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2022-06-01 17:59:13 +0200

    remove tooltip scripts

--- src/app/features/dungeons/dungeons/dungeons.component.ts
+++ src/app/features/dungeons/dungeons/dungeons.component.ts
@@ -1,9 +1,8 @@
-import { AfterViewChecked, AfterViewInit, Component, ElementRef, OnDestroy, OnInit, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { dungeon } from 'src/app/shared/interfaces/dungeons';
 import { SidebarService } from 'src/app/shared/services/sidebar.service';
-import { TooltipService } from 'src/app/shared/services/tooltip.service';
 import { DungeonsService } from '../data/dungeons.service';
 
 @Component({
@@ -17,11 +16,10 @@ export class DungeonsComponent implements OnInit, OnDestroy, AfterViewInit {
   @ViewChildren('item') itemLinks!: QueryList<ElementRef>;
 
   private sub: any;
-  private subTooltip: any;
 
   currentDungeon$: Observable<dungeon>;
 
-  constructor(private sidebarService: SidebarService, private dungeonService: DungeonsService, private tooltipService: TooltipService, private route: ActivatedRoute, private renderer: Renderer2) {
+  constructor(private sidebarService: SidebarService, private dungeonService: DungeonsService, private route: ActivatedRoute) {
     const menu = this.dungeonService.getDungeonsMenu();
     this.sidebarService.setMenuItems(menu);
 
@@ -37,32 +35,8 @@ export class DungeonsComponent implements OnInit, OnDestroy, AfterViewInit {
 
   ngOnDestroy(): void {
     this.sub.unsubscribe();
-    this.subTooltip.unsubscribe();
   }
 
   ngAfterViewInit(): void {
-    //console.log('afterViewInit')
-    this.tooltipService.runScript();
-    this.changeItemlinks();
-
-    this.subTooltip = this.bossList.changes.subscribe(t => {
-      //console.log('CHANGED')
-      this.tooltipService.runScript();
-      this.changeItemlinks();
-    })
-  }
-
-  private changeItemlinks() {
-    console.log('change itemlinks...')
-    this.itemLinks.forEach(item => {
-      console.log(item.nativeElement.getAttribute('href'))
-      //console.log(item.nativeElement.children)
-      //this.renderer.setStyle(item.nativeElement,'padding-left','50px')
-      //this.renderer.setProperty(item.nativeElement, 'innerHTML','<p>TEST</p>')
-      //item.nativeElement.setAttribute('style','test');
-    })
-
-
-    //console.log(this.itemLinks.length);
   }
 }
