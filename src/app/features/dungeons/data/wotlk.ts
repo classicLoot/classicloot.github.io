b00d4ccb2b086fbb73773c87c171a5367d365391
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2022-06-04 05:05:02 +0200

    Update wotlk.ts

--- src/app/features/dungeons/data/wotlk.ts
+++ src/app/features/dungeons/data/wotlk.ts
@@ -20,7 +20,7 @@ export const wotlkDungeons: dungeon[] = [
             { name: 'Keleseth', loot: [35572, 35571, 35570,], lootHeroic: [47241, 37180, 37178, 37179, 37177] },
             { name: 'Skarvald', loot: [35575, 35574, 35573,], lootHeroic: [47241, 37183, 37184, 37182, 37181] },
             { name: 'Ingvar', loot: [35577, 35578, 35576, 33330,], lootHeroic: [47241, 37194, 37193, 37192, 37191, 37189, 37188, 37186, 37190, 41793, 43102] },
-            { name: 'Trash', loot: [35580, 35579, 37197, 37196], lootHeroic: [] },
+            { name: 'Trash', loot: [35580, 35579,], lootHeroic: [37197, 37196] },
         ]
     },
     {
@@ -47,7 +47,7 @@ export const wotlkDungeons: dungeon[] = [
             { name: 'Krikthir', loot: [35657, 35656, 35655,], lootHeroic: [47241, 37218, 37219, 37217, 37216] },
             { name: 'Hadronox', loot: [35660, 35659, 35658,], lootHeroic: [47241, 37222, 37230, 37221, 37220] },
             { name: 'Anubarak', loot: [35663, 35662, 35661, 43411,], lootHeroic: [47241, 37242, 37240, 37241, 37238, 37236, 37237, 37232, 37235, 41796, 43102] },
-            { name: 'Trash', loot: [37243, 37625, 37624], lootHeroic: [] }
+            { name: 'Trash', loot: [], lootHeroic: [37243, 37625, 37624] }
         ]
     },
     {
@@ -62,7 +62,7 @@ export const wotlkDungeons: dungeon[] = [
             { name: 'AmanitarHEROIC', loot: [], lootHeroic: [47241, 43287, 43286, 43285, 43284] },
             { name: 'Jedoga', loot: [43278, 43279, 43277, 21524,], lootHeroic: [47241, 43283, 43280, 43282, 43281, 21524] },
             { name: 'Volazj', loot: [35612, 35613, 35614,], lootHeroic: [47241, 37622, 37623, 37620, 37619, 37616, 37618, 37617, 37615, 41790, 43102] },
-            { name: 'Trash', loot: [35616, 35615, 37625, 37624], lootHeroic: [] }
+            { name: 'Trash', loot: [35616, 35615,], lootHeroic: [37625, 37624] }
         ]
     },
     {
@@ -76,7 +76,7 @@ export const wotlkDungeons: dungeon[] = [
             { name: 'Novos', loot: [35632, 35631, 35630,], lootHeroic: [47241, 37722, 37718, 37721] },
             { name: 'KingDred', loot: [35635, 35634, 35633,], lootHeroic: [47241, 37725, 37724, 37726, 37723] },
             { name: 'Tharonja', loot: [35638, 35637, 35636,], lootHeroic: [47241, 37798, 37791, 37788, 37784, 37735, 37732, 37734, 37733, 41795, 43102] },
-            { name: 'Trash', loot: [35641, 35640, 35639, 37799, 37800, 37801], lootHeroic: [] }
+            { name: 'Trash', loot: [35641, 35640, 35639,], lootHeroic: [37799, 37800, 37801] }
         ]
     },
     {
@@ -93,7 +93,7 @@ export const wotlkDungeons: dungeon[] = [
             { name: 'Moragg', loot: [43387, 43382,], lootHeroic: [47241, 43410, 43408, 43409] },
             { name: 'Lavanthor', loot: [35646, 35645,], lootHeroic: [47241, 37870, 37872, 37871] },
             { name: 'Cyanigosa', loot: [35650, 35651, 35649,], lootHeroic: [47241, 37884, 37886, 43500, 37883, 37876, 37875, 37874, 37873, 41791, 43102] },
-            { name: 'Trash', loot: [35654, 35653, 35652, 35654, 37890, 37891, 35653, 37889, 35652], lootHeroic: [] }
+            { name: 'Trash', loot: [35654, 35653, 35652, 35654,], lootHeroic: [37890, 37891, 35653, 37889, 35652] }
         ]
     },
     {
@@ -108,7 +108,7 @@ export const wotlkDungeons: dungeon[] = [
             { name: 'Moorabi', loot: [35588, 35589, 35587,], lootHeroic: [47241, 37630, 37633, 37632, 37631] },
             { name: 'EckHEROIC', loot: [], lootHeroic: [47241, 43313, 43312, 43311, 43310] },
             { name: 'Galdarah', loot: [43305, 43309, 43306,], lootHeroic: [47241, 37643, 37644, 37645, 37642, 37641, 37640, 37639, 37638, 43102] },
-            { name: 'Trash', loot: [35594, 35593, 37647, 37648, 37646], lootHeroic: [] }
+            { name: 'Trash', loot: [35594, 35593,], lootHeroic: [37647, 37648, 37646] }
         ]
     },
     {
@@ -122,7 +122,7 @@ export const wotlkDungeons: dungeon[] = [
             { name: 'Krystallus', loot: [35673, 35672, 35670,], lootHeroic: [47241, 37652, 37650, 37651] },
             { name: 'Tribunal', loot: [35677, 35676, 35675,], lootHeroic: [47241, 37655, 37656, 37654, 37653] },
             { name: 'Sjonnir', loot: [35679, 35678, 35680,], lootHeroic: [47241, 37669, 37668, 37670, 37667, 37666, 37658, 37657, 37660, 41792, 43102] },
-            { name: 'Trash', loot: [35682, 35683, 35681, 37673, 37672, 37671], lootHeroic: [] }
+            { name: 'Trash', loot: [35682, 35683, 35681,], lootHeroic: [37673, 37672, 37671] }
         ]
     },
     {
@@ -136,7 +136,7 @@ export const wotlkDungeons: dungeon[] = [
             { name: 'Volkhan', loot: [36983, 36985, 36986, 36984,], lootHeroic: [47241, 37840, 37843, 37842, 37841] },
             { name: 'Ionar', loot: [39536, 39657, 39534, 39535,], lootHeroic: [47241, 37846, 37845, 37826, 37844] },
             { name: 'Loken', loot: [36991, 36996, 36992, 36995, 36988, 36993, 36994, 36989, 41799, 43151,], lootHeroic: [47241, 37854, 37853, 37855, 37852, 37851, 37850, 37849, 37848, 41799, 43102] },
-            { name: 'Trash', loot: [36997, 37000, 36999, 37858, 37857, 37856], lootHeroic: [] }
+            { name: 'Trash', loot: [36997, 37000, 36999,], lootHeroic: [37858, 37857, 37856] }
         ]
     },
     {
@@ -165,7 +165,7 @@ export const wotlkDungeons: dungeon[] = [
             { name: 'Palehoof', loot: [37048, 37052, 37051, 37050,], lootHeroic: [47241, 37374, 37373, 37376, 37371] },
             { name: 'Skadi', loot: [37055, 37057, 37056, 37053,], lootHeroic: [47241, 44151, 37389, 37379, 37377, 37384] },
             { name: 'Ymiron', loot: [37067, 37062, 37066, 37058, 37064, 37060, 37065, 37061, 41797,], lootHeroic: [47241, 37408, 37409, 37407, 37401, 37398, 37395, 37397, 37390, 41797, 43102] },
-            { name: 'Trash', loot: [37070, 37069, 37068, 37587, 37590, 37410], lootHeroic: [] }
+            { name: 'Trash', loot: [37070, 37069, 37068,], lootHeroic: [37587, 37590, 37410] }
         ]
     },
     {
@@ -179,7 +179,7 @@ export const wotlkDungeons: dungeon[] = [
             { name: 'Cloudstrider', loot: [36947, 36949, 36948, 36950,], lootHeroic: [47241, 37261, 37262, 37263, 37260] },
             { name: 'Urom', loot: [36954, 36951, 36953, 36952, 21525,], lootHeroic: [47241, 37289, 37288, 37195, 37264, 21525] },
             { name: 'Eregos', loot: [36973, 36971, 36969, 36974, 36961, 36972, 36962, 36975, 41798,], lootHeroic: [47241, 37361, 37363, 37362, 37360, 37291, 37294, 37293, 37292, 43102, 52676] },
-            { name: 'Trash', loot: [36978, 36977, 36976, 37366, 37365, 37290, 37364], lootHeroic: [] }
+            { name: 'Trash', loot: [36978, 36977, 36976,], lootHeroic: [37366, 37365, 37290, 37364] }
         ]
     },
 ]
