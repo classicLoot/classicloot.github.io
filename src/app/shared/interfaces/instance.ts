273fce08af6532684ee84fba2295484ef9a37ad4
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2022-06-10 02:36:17 +0200

    Create instance.ts

---
+++ src/app/shared/interfaces/instance.ts
@@ -0,0 +1,43 @@
+import { wowItem } from "./item"
+
+export interface wowInstance {
+    name: string,
+    link: string,
+    descr?: string,
+    levelMin?: number,
+    levelMax?: number,
+    levelEnter?: number,
+    ilvlMin?: number,
+    ilvlMax?: number,
+    phase: number,
+    size: 5 | 10 | 25 | 40,
+    type?: 'A' | 'B' | 'C',
+    bosses: wowInstanceBoss[]
+}
+
+export interface wowInstanceBoss {
+    name: string,
+    descr?: string,
+    loot?: number[],
+    lootHeroic?: number[],
+    sortedLoot?: wowInstanceLootSorted,
+    sortedLootHeroic?: wowInstanceLootSorted,
+    sortedLootItems?: wowInstanceLootSortedItems,
+    sortedLootHeroicItems?: wowInstanceLootSortedItems
+}
+
+export interface wowInstanceLootSorted {
+    armor: number[],
+    jewelry: number[],
+    weapons: number[],
+    misc: number[],
+    tokens: number[]
+}
+
+export interface wowInstanceLootSortedItems {
+    armor: wowItem[],
+    jewelry: wowItem[],
+    weapons: wowItem[],
+    misc: wowItem[],
+    tokens: wowItem[]
+}
