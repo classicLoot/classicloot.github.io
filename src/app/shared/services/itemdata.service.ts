3aae9025dee0c3f62508752d9ea7b0e7c0d55cb5
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2022-06-13 00:12:22 +0200

    remove raids/dungeons

--- src/app/shared/services/itemdata.service.ts
+++ src/app/shared/services/itemdata.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs';
 import { wowItem } from '../interfaces/item';
 
 @Injectable({
