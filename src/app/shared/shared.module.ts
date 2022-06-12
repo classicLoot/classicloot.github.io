1ee2b660ddf4a6e534b2a63ea7538a413407440e
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2022-06-13 00:23:45 +0200

    Update shared.module.ts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { WowItemComponent } from './components/wow-item/wow-item.component';
-import { WowItemTooltipComponent } from './components/wow-item-tooltip/wow-item-tooltip.component';
-import { ItemSubtextPipe } from './pipes/item-subtext.pipe';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { SidebarItemComponent } from './components/sidebar-item/sidebar-item.component';
 import { SidebarMenuComponent } from './components/sidebar-menu/sidebar-menu.component';
-import { RouterModule } from '@angular/router';
 import { WowItemLazyComponent } from './components/wow-item-lazy/wow-item-lazy.component';
+import { WowItemTooltipComponent } from './components/wow-item-tooltip/wow-item-tooltip.component';
+import { WowItemComponent } from './components/wow-item/wow-item.component';
+import { ItemSubtextPipe } from './pipes/item-subtext.pipe';
 
 
 @NgModule({
