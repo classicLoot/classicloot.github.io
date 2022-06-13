5639c0ed96e22317691256bbd01892e04ec4553f
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2022-06-13 17:03:46 +0200

    Create achievment.ts

---
+++ src/app/shared/interfaces/achievment.ts
@@ -0,0 +1,12 @@
+export interface wowAchievment {
+    id: number,
+    instanceID: number,
+    faction: number,
+    supercedes: number,
+    name: string,
+    descr: string,
+    category: number,
+    points: number,
+    iconID: number,
+    reward: string,
+}
