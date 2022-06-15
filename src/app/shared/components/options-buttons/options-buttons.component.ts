4e12fe52b3a5907c297e08a5273e9c2d70f82bec
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2022-06-15 15:53:30 +0200

    reroute

--- src/app/shared/components/options-buttons/options-buttons.component.ts
+++ src/app/shared/components/options-buttons/options-buttons.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { OptionsStoreService } from '../../services/options-store.service';
 import { wowDifficulty, wowFaction, wowSize } from '../../types/options';
@@ -19,7 +20,7 @@ export class OptionsButtonsComponent implements OnInit {
   size$: Observable<wowSize>;
   difficulty$: Observable<wowDifficulty>;
 
-  constructor(private optionsStore: OptionsStoreService) {
+  constructor(private optionsStore: OptionsStoreService, private router: Router) {
     this.faction$ = this.optionsStore.faction$;
     this.size$ = this.optionsStore.size$;
     this.difficulty$ = this.optionsStore.difficulty$;
@@ -37,6 +38,30 @@ export class OptionsButtonsComponent implements OnInit {
   public updateSize(value: wowSize, old: wowSize): void {
     if (value != old) {
       this.optionsStore.updateSize(value);
+
+      const size = this.optionsStore.getSize();
+
+      // 10 <=> 25
+      const url = this.router.routerState.snapshot.url;
+
+      // e.g. Naxx10
+      if (url.endsWith('10')) {
+        if (size === 10) {
+        }
+        else if (size === 25) {
+          const newURL = url.replace('10', '25');
+          this.router.navigate([newURL]);
+        }
+      }
+      // e.g. Naxx25
+      else if (url.endsWith('25')) {
+        if (size === 25) {
+        }
+        else if (size === 10) {
+          const newURL = url.replace('25', '10');
+          this.router.navigate([newURL]);
+        }
+      }
     }
   }
 
