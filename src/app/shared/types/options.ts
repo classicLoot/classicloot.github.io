db59cddf1271bb12e060a6df40e89c286e183fea
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2022-07-02 05:56:39 +0200

    Update options.ts

--- src/app/shared/types/options.ts
+++ src/app/shared/types/options.ts
@@ -1,3 +1,5 @@
 export type wowFaction = 'Alliance' | 'Horde';
 export type wowDifficulty = 'Normal' | 'Heroic';
 export type wowSize = 5 | 10 | 25 | 40;
+
+export type wowClass = 'Deathknight' | 'Druid' | 'Hunter' | 'Mage' | 'Paladin' | 'Priest' | 'Rogue' | 'Shaman' | 'Warlock' | 'Warrior';
