691b0d188dbea75480bcb3d8c51f30d18d3b6eab
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2022-07-02 23:12:59 +0200

    Better pipe (#53)
    * test pipe
    
    * pipe class check
    
    * more tests

---
+++ src/scraper/packages/pipe.ts
@@ -0,0 +1,20 @@
+import { ItemVisibilityPipe } from '../../app/shared/pipes/item-visibility.pipe';
+import { wowClass } from '../../app/shared/types/options';
+import { readIDsAsItems } from '../helper';
+
+//testItemVisPipe([39275, 39274, 40625, 40626, 40627, 45634, 40612], ['Druid']);
+testItemVisPipe([39275], ['Deathknight']);
+
+
+function testItemVisPipe(ids: number[], classes: wowClass[]) {
+    const itemVisPipe = new ItemVisibilityPipe();
+
+    const items = readIDsAsItems(ids);
+    items.forEach(item => {
+        classes.forEach(wowclass => {
+            const isVisible = itemVisPipe.transform(item, wowclass);
+            console.log(item.name, '=>', wowclass, isVisible);
+        })
+    })
+}
+
