504e0e96ecab4af7946a3f7357ffdb565447cd26
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2022-08-21 18:32:38 +0200

    new Sidebar

---
+++ src/app/features/collectionsx/collectionsx.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CollectionsxRoutingModule } from './collectionsx-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CollectionsxRoutingModule
+  ]
+})
+export class CollectionsxModule { }
