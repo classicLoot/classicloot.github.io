4265541f8730e89384e13d6a428ae68e8d130824
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2022-09-04 21:53:58 +0200

    wow-Quest-Lazy component

--- src/app/shared/components/wow-quest-lazy/wow-quest-lazy.component.ts
+++ src/app/shared/components/wow-quest-lazy/wow-quest-lazy.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { wowQuest } from '../../interfaces/collection';
+import { wowItem } from '../../interfaces/item';
+import { GlobalStoreService } from '../../services/global-store.service';
+import { ModalService } from '../../services/modal.service';
+import { TooltipService } from '../../services/tooltip.service';
 
 @Component({
   selector: 'app-wow-quest-lazy',
@@ -10,9 +15,35 @@ export class WowQuestLazyComponent implements OnInit {
 
   @Input() quest!: wowQuest;
 
-  constructor() { }
+  mobile$: Observable<boolean>;
+
+  constructor(private tooltipService: TooltipService, private globalStore: GlobalStoreService, private modalService: ModalService) {
+    this.mobile$ = this.globalStore.mobile$;
+  }
 
   ngOnInit(): void {
+  }
+
+  public onMouseEvent(e: MouseEvent, type: 'move' | 'enter' | 'leave') {
+    //console.log(type, item.name);
+    //this.tooltipService.onMouseEvent(e, type, this.item);
+    const item: wowItem = {
+      id: 0,
+      quality: 0,
+      link: this.quest.link!,
+      icon: 'inv_misc_book_11',
+      name: 'Testname',
+      wowClass: 0,
+      wowSubClass: 0,
+      ilvl: 69,
+      slot: 0,
+      htmlTooltip: this.quest.tooltip!
+    }
+    this.tooltipService.onMouseEvent(e, type, item);
+  }
+
+  public clickMobile(e: MouseEvent) {
+    // this.modalService.setItem(this.item);
   }
 
 }
