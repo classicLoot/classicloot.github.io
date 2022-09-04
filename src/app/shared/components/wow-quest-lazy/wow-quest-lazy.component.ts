f9ea7da6ae390a940ad782930f1d504418606184
Karl-HeinzSchneider <56960197+Karl-HeinzSchneider@users.noreply.github.com>
2022-09-05 00:20:26 +0200

    add preQuests

--- src/app/shared/components/wow-quest-lazy/wow-quest-lazy.component.ts
+++ src/app/shared/components/wow-quest-lazy/wow-quest-lazy.component.ts
@@ -24,20 +24,20 @@ export class WowQuestLazyComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  public onMouseEvent(e: MouseEvent, type: 'move' | 'enter' | 'leave') {
+  public onMouseEvent(e: MouseEvent, type: 'move' | 'enter' | 'leave', quest: wowQuest) {
     //console.log(type, item.name);
     //this.tooltipService.onMouseEvent(e, type, this.item);
     const item: wowItem = {
       id: 0,
       quality: 0,
-      link: this.quest.link!,
+      link: quest.link!,
       icon: 'inv_misc_book_11',
       name: 'Testname',
       wowClass: 0,
       wowSubClass: 0,
       ilvl: 69,
       slot: 0,
-      htmlTooltip: this.quest.tooltip!
+      htmlTooltip: quest.tooltip!
     }
     this.tooltipService.onMouseEvent(e, type, item);
   }
